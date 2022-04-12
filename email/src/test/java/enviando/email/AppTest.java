3a0849d9ed16f811439ee6a0ac972f28030f232d
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-04-12 10:13:28 -0300

    Configurando conexão com servidor Gmail

--- email/src/test/java/enviando/email/AppTest.java
+++ email/src/test/java/enviando/email/AppTest.java
@@ -2,6 +2,8 @@ package enviando.email;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 /**
@@ -9,12 +11,19 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	
+	@Test //marcado como teste para poder rodar o código e efetuar teste, não sendo necessário método main
+	public void testeEmail() {
+		
+		//verificar as configurações SMTP do seu provedor e email;
+		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true"); //autorização
+		properties.put("mail.smtp.starttls", "true"); //autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com"); //servidor Gmail Google
+		properties.put("mail.smtp.port", "465"); //porta do servidor
+		properties.put("mail.smtp.socketFactory.port", "465"); //especifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //classe socket de conesão ao SMTP
+		
+	}
 }
