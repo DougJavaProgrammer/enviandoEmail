9758696e3b212517fb23e87b5d4ba90d27586d44
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-04-12 14:01:25 -0300

    Configurando a conexão com o servidor GMAIL

--- email/src/test/java/enviando/email/AppTest.java
+++ email/src/test/java/enviando/email/AppTest.java
@@ -1,29 +1,53 @@
 package enviando.email;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+
+	private String userName = "dougmoroli@gmail.com";
+	private String password = "endehe76";
 	
-	@Test //marcado como teste para poder rodar o código e efetuar teste, não sendo necessário método main
+	
+	@Test // marcado como teste para poder rodar o código e efetuar teste, não sendo
+			// necessário método main
+	
 	public void testeEmail() {
-		
-		//verificar as configurações SMTP do seu provedor e email;
-		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true"); //autorização
-		properties.put("mail.smtp.starttls", "true"); //autenticação
-		properties.put("mail.smtp.host", "smtp.gmail.com"); //servidor Gmail Google
-		properties.put("mail.smtp.port", "465"); //porta do servidor
-		properties.put("mail.smtp.socketFactory.port", "465"); //especifica a porta a ser conectada pelo socket
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //classe socket de conesão ao SMTP
-		
+
+		try {
+			// verificar as configurações SMTP do seu provedor e email;
+
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true"); // autorização
+			properties.put("mail.smtp.starttls", "true"); // autenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com"); // servidor Gmail Google
+			properties.put("mail.smtp.port", "465"); // porta do servidor
+			properties.put("mail.smtp.socketFactory.port", "465"); // especifica a porta a ser conectada pelo socket
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // classe socket de
+																								// conesão ao SMTP
+
+			Session session = Session.getDefaultInstance(properties, new Authenticator() {
+
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+			
+			System.out.println(session);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
