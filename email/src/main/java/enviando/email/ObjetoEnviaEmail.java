d26bd45fa2aa137b305d32309accc16c013e8ea6
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-04-12 17:43:02 -0300

    criando a classe ObjetoEnviaEmail PS: construtor não está funcionando

---
+++ email/src/main/java/enviando/email/ObjetoEnviaEmail.java
@@ -0,0 +1,64 @@
+package enviando.email;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
+
+public class ObjetoEnviaEmail {
+
+	private String userName = JOptionPane.showInputDialog("Digite o usuário do GMAIL: ");
+	private String password = JOptionPane.showInputDialog("Digite a senha de " + userName);
+	private String listaDestinatarios = JOptionPane.showInputDialog("Digite o email de destino: ");	
+	private String nomeRemetente = "";
+	private String assuntoEmail = "";
+	private String mensagemEmail = "";
+	
+	public ObjetoEnviaEmail(String listaDestinario, String nomeRemetente, String assuntoEmail, String mensagemEmail) {
+		this.listaDestinatarios = listaDestinario;
+		this.nomeRemetente = nomeRemetente;
+		this.assuntoEmail = assuntoEmail;
+		this.mensagemEmail = mensagemEmail;
+	}
+	
+	public void enviarEmail() throws Exception {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.ssl.trust", "*"); // autenticação SSL
+		properties.put("mail.smtp.auth", "true"); // autorização
+		properties.put("mail.smtp.starttls", "true"); // autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com"); // servidor Gmail Google
+		properties.put("mail.smtp.port", "465"); // porta do servidor
+		properties.put("mail.smtp.socketFactory.port", "465"); // especifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // classe socket de
+																							// conesão ao SMTP
+
+		Session session = Session.getDefaultInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+
+		Address[] toUser = InternetAddress.parse(listaDestinatarios);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName, nomeRemetente)); // origem do email, ou seja, quem
+																					// envia
+		message.setRecipients(Message.RecipientType.TO, toUser); // email de destino
+		message.setSubject(assuntoEmail); // assunto do email
+		message.setText(mensagemEmail);
+
+		Transport.send(message);
+		JOptionPane.showInternalMessageDialog(null, "E-mail enviado com sucesso");
+	}
+
+}
--- email/src/test/java/enviando/email/AppTest2.java
+++ email/src/test/java/enviando/email/AppTest2.java
@@ -19,49 +19,21 @@ import org.junit.Test;
  */
 public class AppTest2 {
 
-	private String userName = JOptionPane.showInputDialog("Digite o usuário do GMAIL: ");
-	private String password = JOptionPane.showInputDialog("Digite a senha de " + userName);
-
 	@Test // marcado como teste para poder rodar o código e efetuar teste, não sendo
 			// necessário método main
 
 	public void testeEmail() {
 
 		try {
-			// verificar as configurações SMTP do seu provedor e email;
 
-			Properties properties = new Properties();
-			properties.put("mail.smtp.ssl.trust","*"); //autenticação SSL
-			properties.put("mail.smtp.auth", "true"); // autorização
-			properties.put("mail.smtp.starttls", "true"); // autenticação
-			properties.put("mail.smtp.host", "smtp.gmail.com"); // servidor Gmail Google
-			properties.put("mail.smtp.port", "465"); // porta do servidor
-			properties.put("mail.smtp.socketFactory.port", "465"); // especifica a porta a ser conectada pelo socket
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // classe socket de
-																								// conesão ao SMTP
-
-			Session session = Session.getDefaultInstance(properties, new Authenticator() {
-
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-
-					return new PasswordAuthentication(userName, password);
-				}
-			});
-
-			Address[] toUser = InternetAddress.parse(JOptionPane.showInputDialog("Digite o destinatário: "));
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName, "Douglas JavaProgrammer")); // origem do email, ou seja, quem envia
-			message.setRecipients(Message.RecipientType.TO, toUser); // email de destino
-			message.setSubject(JOptionPane.showInputDialog("Digite o assunto: ")); // assunto do email
-			message.setText(JOptionPane.showInputDialog("Digite a mensagem: "));
-
-			Transport.send(message);
-			JOptionPane.showInternalMessageDialog(null, "E-mail enviado com sucesso");
+			ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail(JOptionPane.showInputDialog("Destinatátio: "),
+					JOptionPane.showInputDialog("Nome do remetente: "), JOptionPane.showInputDialog("Assunto: "),
+					JOptionPane.showInputDialog("Mensagem: "));
+			
+			enviaEmail.enviarEmail();
 
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Falha catastrófica: \n"+ e.getMessage());
+			JOptionPane.showMessageDialog(null, "Falha catastrófica: \n" + e.getMessage());
 			e.printStackTrace();
 
 		}
