f8c1d4d696b85335a3dd21d2cd1dd89b05739705
Douglas Oliveira <dougmoroli@gmail.com>
2022-04-14 17:44:34 -0300

    email customizado com HTML

--- email/src/test/java/enviando/email/AppTest2.java
+++ email/src/test/java/enviando/email/AppTest2.java
@@ -21,7 +21,10 @@ public class AppTest2 {
 			stringBuilderTextoEmail.append("Você está recebendo acesso ao curso de Java <br/><br/>");
 			stringBuilderTextoEmail.append("Para ter acesso, clique no link abaixo. <br/><br/>");
 			stringBuilderTextoEmail.append(
-					"<a target= \"_blank\" href=\"www.google.com.br\" style=\"color:#2525a7;padding: 14px 25px; text-align:center;text-decoration: none;display:inline-block;border-radius:30px; font-size:20px; font-family:courier; border:3px solid green;background-color:#99DA39;\">Acessar Portal </a>");
+					"<a target= \"_blank\" href=\"www.google.com.br\" style=\"color:#2525a7;padding: 14px 25px;"
+					+ " text-align:center;text-decoration: none;display:inline-block;border-radius:30px;"
+					+ " font-size:20px; font-family:courier; border:3px solid green;background-color:#99DA39;"
+					+ "\">Acessar Portal </a>");
 
 			ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail(JOptionPane.showInputDialog("Digite o usuário GMAIL: "),
 					JOptionPane.showInputDialog("Digite a senha do usuário: "),
